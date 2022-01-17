58cc7f30b93aa096856023eea00fa4cfe1c11b09
libinglong002 <libinglong002@ke.com>
2022-01-17 22:42:58 +0800

    up

---
+++ src/main/java/com/lbl/wiki/wikipersonal/WikiPersonalApplication.java
@@ -0,0 +1,13 @@
+package com.lbl.wiki.wikipersonal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WikiPersonalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WikiPersonalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lbl/wiki/wikipersonal/WikiPersonalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lbl.wiki.wikipersonal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WikiPersonalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
